e6af517489837391043ec76a5e1619df3645177b
Douglas Bastos <dautodouglasdauto@gmail.com>
2023-12-09 15:24:12 -0300

    Modificado o componente Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,27 @@
 import logo from '../../assets/img/logo.svg';
 import search from '../../assets/img/search.svg';
 import bag from '../../assets/img/bag.svg';
+import './styles.css';
 
 export default function Header() {
     return (
-        <header className='container-fluid'>
-            <div className='row'>
+        <header className='container-fluid header'>
+            <div className='row align-items-center'>
                 <img src={logo} alt="Logo" className='col-2' />
 
-                <div className='col-10 align-items-center'>
-                    <img src={search} alt="Pesquisa" />
-                    <input type="text" />
-                    <img src={bag} alt="Bolsa" />
+                <div className='col-10'>
+                    <div className='row'>
+                        <div className='col-11 input-search'>
+                            <img src={search} alt="Pesquisa" className='col-1 search' />
+                            <input type="text" placeholder='Pesquise fontes' className='container-fluid col-11 input' />
+                        </div>
+
+                        <img src={bag} className='col-1 bag' alt="Bolsa" />
+                    </div>
+                </div>
+
+                <div className='w-100'>
+                    <button className='button-filter'>Filtros</button>
                 </div>
             </div>
         </header>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
+import './styles/variables.css';
+import './styles/global-styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
